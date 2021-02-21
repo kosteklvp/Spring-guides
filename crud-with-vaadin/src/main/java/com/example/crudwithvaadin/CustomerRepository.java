99e1551ffc58c47f26d9a99cb92050887476954e
Piotr Kostański <kostek.lvp@gmail.com>
2021-02-21 19:38:46 +0100

    po vaadin

---
+++ crud-with-vaadin/src/main/java/com/example/crudwithvaadin/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.crudwithvaadin;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	List<Customer> findByLastNameStartsWithIgnoreCase(String lastName);
+}
